aee0e973a70b51c07a0e677d613c71f737b49d61
brandon-jimenez <36610687+brandon-jimenez@users.noreply.github.com>
2022-01-03 17:37:31 -0600

    fixes & uploads
    Fixing resume linking
    Uploading images
    Fixing home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-        
+        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
        
       </div>
@@ -10,7 +10,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="https://source.unsplash.com/random/720x400"
+          src="/images/uploads/Bliss.jpg"
         />
       </div>
     </div>
